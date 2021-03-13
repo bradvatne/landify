18f1744427ed9e19b1b698d751b8435aef5aa9ce
Brad Vatne <brad.vatne@gmail.com>
2021-03-13 18:50:58 +0800

    destructure header props

--- containers/Header.tsx
+++ containers/Header.tsx
@@ -1,30 +1,28 @@
-import styles from "../styles/Nav.module.scss";
+import s from "../styles/Nav.module.scss";
 
 export default function Header({ headerContent }) {
+  const { menu_links, nav_logo, social_links } = headerContent;
   return (
-    <div className={styles.nav_wrapper}>
-    <nav className={styles.nav}>
-      <ul className={styles.menu_links}>
-        {headerContent.menu_links.map((item, index) => (
-          <li key={index} className={styles.menu_link_item}>
-            <a href={item.url}>{item.label}</a>
-          </li>
-        ))}
-      </ul>
-      <a href="/" className={styles.nav_logo_container}>
-        <img
-          alt={headerContent.nav_logo.alt}
-          src={headerContent.nav_logo.image}
-        />
-      </a>
-      <div className={styles.social_links}>
-        {headerContent.social_links.map((item, index) => (
-          <a href={item.url} key={index}>
-            <img alt={item.brand} src={item.icon} />
-          </a>
-        ))}
-      </div>
-    </nav>
+    <div className={s.nav_wrapper}>
+      <nav className={s.nav}>
+        <ul className={s.menu_links}>
+          {menu_links.map((item, index) => (
+            <li key={index} className={s.menu_link_item}>
+              <a href={item.url}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+        <a href="/" className={s.nav_logo_container}>
+          <img alt={nav_logo.alt} src={nav_logo.image} />
+        </a>
+        <div className={s.social_links}>
+          {social_links.map((item, index) => (
+            <a href={item.url} key={index}>
+              <img alt={item.brand} src={item.icon} />
+            </a>
+          ))}
+        </div>
+      </nav>
     </div>
   );
 }
