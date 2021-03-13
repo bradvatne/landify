132cee18ecfccc85d8f0dfcfb5f2552feb328e4c
Brad Vatne <brad.vatne@gmail.com>
2021-03-13 19:46:40 +0800

    change var names

--- containers/Brands.tsx
+++ containers/Brands.tsx
@@ -1,10 +1,10 @@
-import styles from "../styles/Brands.module.scss";
+import s from "../styles/Brands.module.scss";
 import { IBrands } from "../types";
 
 export default function Brands({ brandsContent }: IBrands) {
   return (
-    <div className={styles.brand_logos_wrapper}>
-      <div className={styles.brand_logos_container}>
+    <div className={s.brand_logos_wrapper}>
+      <div className={s.brand_logos_container}>
         {brandsContent.map((item, index) => (
           <img key={index} src={item.logo} alt={item.brand} />
         ))}
