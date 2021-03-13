95db7ff7f61398d9254cba6cd59f4dddd0e86762
Brad Vatne <brad.vatne@gmail.com>
2021-03-13 18:48:56 +0800

    destructure hero props

--- containers/Hero.tsx
+++ containers/Hero.tsx
@@ -1,32 +1,29 @@
-import styles from "../styles/Hero.module.scss";
+import s from "../styles/Hero.module.scss";
 import { Header, Brands } from "./";
 
 export default function Hero({ heroContent, headerContent, brandsContent }) {
+  const { heading, lead, button, image } = heroContent;
   return (
-    <div className={styles.hero_container}>
+    <div className={s.hero_container}>
       <Header headerContent={headerContent} />
-      <svg className={styles.circle_purple} height="740" width="740">
+      <svg className={s.circle_purple} height="740" width="740">
         <circle cx="370" cy="370" r="370" />
       </svg>
-      <svg className={styles.circle_orange} height="72" width="72">
+      <svg className={s.circle_orange} height="72" width="72">
         <circle cx="36" cy="36" r="36" />
       </svg>
-      <svg className={styles.circle_green} height="72" width="72">
+      <svg className={s.circle_green} height="72" width="72">
         <circle cx="36" cy="36" r="36" />
       </svg>
-      <div className={styles.hero_content}>
-        <div className={styles.hero_cta}>
-          <h1>{heroContent.heading}</h1>
-          <p className={styles.lead}>{heroContent.lead}</p>
-          <a href={heroContent.button.url}>
-            <button className={styles.cta}>{heroContent.button.label}</button>
+      <div className={s.hero_content}>
+        <div className={s.hero_cta}>
+          <h1>{heading}</h1>
+          <p className={s.lead}>{lead}</p>
+          <a href={button.url}>
+            <button className={s.cta}>{button.label}</button>
           </a>
         </div>
-        <img
-          src={heroContent.image.url}
-          alt={heroContent.image.alt}
-          className={styles.hero_image}
-        />
+        <img src={image.url} alt={image.alt} className={s.hero_image} />
       </div>
       <Brands brandsContent={brandsContent} />
     </div>
