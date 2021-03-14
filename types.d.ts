4d9875f65f1bce47daa6193d214a54399f001ea0
Brad Vatne <brad.vatne@gmail.com>
2021-03-14 17:04:12 +0800

    add more types

--- types.d.ts
+++ types.d.ts
@@ -94,22 +94,52 @@ export type TReviews = {
     }
 }
 
-export type TStats = {
+type Stat = {
+    title: string,
+    lead: string,
+    icon: string
+}
 
+export type TStats = {
+    title: string,
+    lead: string,
+    stats: Stat[]
 }
 
 export type TFeatureLeft = {
-
+    image: string,
+    title: string,
+    lead: string,
+    a: {
+        label: string,
+        url: string
+    }
 }
 
 export type TFeatureCenter = {
-
+    title: string,
+    lead: string,
+    image: string
 }
 
 export type TCta = {
-    
+    title: string,
+    lead: string,
+    cta: string,
+    button1: string,
+    button2: string,
+    image1: string,
+    image2: string
+}
+
+type Link = {
+    link: string,
+    label: string
 }
 
 export type TFooter = {
-
+    logo: string,
+    download: Link,
+    liscence: Link,
+    nav: Link[]
 }
