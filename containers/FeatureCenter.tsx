605939f32d0c41e5ded7242aa42bcf9d7eca48a9
Brad Vatne <brad.vatne@gmail.com>
2021-03-15 22:07:36 +0800

    responsiveness

--- containers/FeatureCenter.tsx
+++ containers/FeatureCenter.tsx
@@ -1,16 +1,25 @@
-import s from '../styles/FeatureCenter.module.scss'
+import s from "../styles/FeatureCenter.module.scss";
+import Image from "next/image";
 
-export default function FeatureCenter({featureCenterContent}) {
-    const {title, lead, image} = featureCenterContent;
-    return <div className={s.wrapper}>
-        <div className={s.left}>
-            <h3 className={s.title}>{title}</h3>
-        </div>
-        <div className={s.right}>
-            <p className={s.lead}>{lead}</p>
-        </div>
-        <div className={s.image}>
-            <img src={image} className={s.img} />
-        </div>
+export default function FeatureCenter({ featureCenterContent }) {
+  const { title, lead, image } = featureCenterContent;
+  return (
+    <div className={s.wrapper}>
+      <div className={s.left}>
+        <h3 className={s.title}>{title}</h3>
+      </div>
+      <div className={s.right}>
+        <p className={s.lead}>{lead}</p>
+      </div>
+      <div className={s.image}>
+        <Image
+          height={400}
+          width={1000}
+          layout="responsive"
+          src={image}
+          className={s.img}
+        />
+      </div>
     </div>
+  );
 }
