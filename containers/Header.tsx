04c3fec1e9f381ea36545fd28a13c05c3fd1e488
Brad Vatne <brad.vatne@gmail.com>
2021-03-15 18:26:49 +0800

    nav + hero responsive

--- containers/Header.tsx
+++ containers/Header.tsx
@@ -1,19 +1,36 @@
 import s from "../styles/Nav.module.scss";
+import Image from "next/image";
 import { IHeader } from "../types";
+import { useState } from "react";
 
 export default function Header({ headerContent }: IHeader) {
   const { menu_links, nav_logo, social_links } = headerContent;
+  const [showNav, setShowNav] = useState(false);
+
+  function toggleNav() {
+    setShowNav(!showNav);
+  }
+
+  function getNavVisibility() {
+    if (showNav) {
+      return { display: "none" };
+    } else return {};
+  }
+
   return (
-    <div className={s.nav_wrapper}>
+    <div className={s.wrapper}>
       <nav className={s.nav}>
-        <ul className={s.menu_links}>
+        <ul
+          style={showNav ? { display: "flex" } : { flexBasis: "33%" }}
+          className={s.menu_links}
+        >
           {menu_links.map((link, index) => (
             <li key={index} className={s.menu_link_item}>
               <a href={link.url}>{link.label}</a>
             </li>
           ))}
         </ul>
-        <a href="/" className={s.nav_logo_container}>
+        <a href="/" className={s.logo_container}>
           <img alt={nav_logo.alt} src={nav_logo.image} />
         </a>
         <div className={s.social_links}>
@@ -23,6 +40,13 @@ export default function Header({ headerContent }: IHeader) {
             </a>
           ))}
         </div>
+        <button
+          className={s.toggle_nav}
+          aria-label="Open Navigation"
+          onClick={() => toggleNav()}
+        >
+          <Image width={14} height={8} src="/images/chevron-down.svg" />
+        </button>
       </nav>
     </div>
   );
--- containers/Hero.tsx
+++ containers/Hero.tsx
@@ -1,11 +1,16 @@
 import s from "../styles/Hero.module.scss";
-import { IHero} from "../types";
+import { IHero } from "../types";
 import { Header, Brands } from "./";
+import Image from "next/image";
 
-export default function Hero({ heroContent, headerContent, brandsContent }: IHero) {
+export default function Hero({
+  heroContent,
+  headerContent,
+  brandsContent,
+}: IHero) {
   const { heading, lead, button, image } = heroContent;
   return (
-    <div className={s.hero_container}>
+    <div className={s.wrapper}>
       <Header headerContent={headerContent} />
       <svg className={s.circle_purple} height="740" width="740">
         <circle cx="370" cy="370" r="370" />
@@ -16,15 +21,24 @@ export default function Hero({ heroContent, headerContent, brandsContent }: IHer
       <svg className={s.circle_green} height="72" width="72">
         <circle cx="36" cy="36" r="36" />
       </svg>
-      <div className={s.hero_content}>
-        <div className={s.hero_cta}>
+      <div className={s.content}>
+        <div className={s.left}>
           <h1>{heading}</h1>
-          <p className={s.lead}>{lead}</p>
+          <p>{lead}</p>
           <a href={button.url}>
             <button className={s.cta}>{button.label}</button>
           </a>
         </div>
-        <img src={image.url} alt={image.alt} className={s.hero_image} />
+        <div className={s.image_container}>
+          <Image
+            height={387}
+            width={518}
+            layout="responsive"
+            src={image.url}
+            alt={image.alt}
+            className={s.image}
+          />
+        </div>
       </div>
       <Brands brandsContent={brandsContent} />
     </div>
